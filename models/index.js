02fb267011668854d7c9bbc23d3680d60c917398
Jessica Almand <jlalmand27@gmail.com>
2022-11-10 17:17:16 -0500

    Finished index.js and linking models with foreign keys

--- models/index.js
+++ models/index.js
@@ -4,6 +4,22 @@ const User = require('./user');
 const Post = require('./post');
 
 Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
-})
 module.exports = { Comment, Home, User, Post };
