2c13811e40f121e141def13c8c757e5d01ea90ae
Jessica Almand <jlalmand27@gmail.com>
2022-11-10 16:58:03 -0500

    Got main page to show up and login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -61,7 +61,7 @@ router.get('/home', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render('single-post', {
       ...user,
       logged_in: true
     });
@@ -73,7 +73,7 @@ router.get('/home', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/home');
+    res.redirect('/');
     return;
   }
 
